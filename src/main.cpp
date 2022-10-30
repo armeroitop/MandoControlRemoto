df24df8e2c6e875f8f7e384d5435207cbd66ed48
armeroitop <armeroitop@gmail.com>
2022-10-30 20:36:24 +0100

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <Cara.h>
+#include <Ruedas.h>
+//#include <Antena.h>
 
 Cara *p_cara = new Cara();
 
+//Antena *p_antena = new Antena();
+
 void setup(void)
 {
-  
+    //p_antena->setup();
 }
 
 void loop() 
 {
-    
+    //p_ruedas->update();
     p_cara->update();    
+    //p_antena->update();
 }
